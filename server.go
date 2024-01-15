6f6cffe2fdf498d88143341b0619e11da9bd40ee
srivier <soleane.rivier@ynov.com>
2024-01-15 11:57:16 +0100

    feat: Add item and flower

--- server.go
+++ server.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -10,21 +9,27 @@ import (
 )
 
 type Case struct {
-	X    int
-	Y    int
-	Type string
+	X      int
+	Y      int
+	Type   string
+	Type2  string
+	Type3  string
+	Item   bool
+	Flower bool
 }
 
 type Grille struct {
 	XTaille int
 	YTaille int
 	Grid    [][]Case
+	Couche  int
+	Start   int
+	NbrItem int
 }
 
 func main() {
 	var grille Grille
-	grille.Grid = append(grille.Grid, []Case{{0, 0, "VIDE"}})
-	printGrille(grille.Grid)
+	grille.Grid = append(grille.Grid, []Case{{0, 0, "VIDE", "VIDE", "VIDE", false, false}})
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		Home(w, r, grille)
@@ -34,6 +39,9 @@ func main() {
 	})
 	http.HandleFunc("/affiche", func(w http.ResponseWriter, r *http.Request) {
 		Affiche(w, r, &grille)
+	})
+	http.HandleFunc("/couche", func(w http.ResponseWriter, r *http.Request) {
+		Couche(w, r, &grille)
 	})
 	fs := http.FileServer(http.Dir("static/"))
 	http.Handle("/static/", http.StripPrefix("/static", fs))
@@ -59,7 +67,7 @@ func TailleGrille(w http.ResponseWriter, r *http.Request, grille *Grille) {
 		slices := func() []Case {
 			var slice []Case
 			for i := 0; i <= grille.XTaille; i++ {
-				slice = append(slice, Case{i, grille.YTaille, "VIDE"})
+				slice = append(slice, Case{i, grille.YTaille, "VIDE", "VIDE", "VIDE", false, false})
 			}
 			return slice
 		}
@@ -67,19 +75,10 @@ func TailleGrille(w http.ResponseWriter, r *http.Request, grille *Grille) {
 	case "x+":
 		grille.XTaille++
 		for i := 0; i <= grille.YTaille; i++ {
-			grille.Grid[i] = append(grille.Grid[i], Case{grille.XTaille, i, "VIDE"})
+			grille.Grid[i] = append(grille.Grid[i], Case{grille.XTaille, i, "VIDE", "VIDE", "VIDE", false, false})
 		}
 	}
 	http.Redirect(w, r, "/", http.StatusSeeOther)
-}
-
-func printGrille(grille [][]Case) {
-	for _, item := range grille {
-		for _, item2 := range item {
-			fmt.Print(item2)
-		}
-		fmt.Println("")
-	}
 }
 
 func Affiche(w http.ResponseWriter, r *http.Request, grille *Grille) {
@@ -87,25 +86,76 @@ func Affiche(w http.ResponseWriter, r *http.Request, grille *Grille) {
 	tab := strings.Split(affiche, " ")
 	x, _ := strconv.Atoi(tab[0])
 	y, _ := strconv.Atoi(tab[1])
-	ChangeType(&grille.Grid[y][x])
+	ChangeType(&grille.Grid[y][x], grille.Couche)
+	grille.NbrItem = CaculNombreItem(grille.Grid)
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
-func ChangeType(cases *Case) {
-	switch cases.Type {
-	case "VIDE":
-		cases.Type = "ONTOP"
-	case "ONTOP":
-		cases.Type = "ONBOTTOM"
-	case "ONBOTTOM":
-		cases.Type = "ONLEFT"
-	case "ONLEFT":
-		cases.Type = "ONRIGHT"
-	case "ONRIGHT":
-		cases.Type = "CENTER"
-	case "CENTER":
-		cases.Type = "BLOCK"
-	case "BLOCK":
-		cases.Type = "VIDE"
+func ChangeType(cases *Case, couche int) {
+	switch couche {
+	case 0:
+		switch cases.Type {
+		case "VIDE":
+			cases.Type = "ONTOP"
+		case "ONTOP":
+			cases.Type = "ONBOTTOM"
+		case "ONBOTTOM":
+			cases.Type = "ONLEFT"
+		case "ONLEFT":
+			cases.Type = "ONRIGHT"
+		case "ONRIGHT":
+			cases.Type = "CENTER"
+		case "CENTER":
+			cases.Type = "BLOCK"
+		case "BLOCK":
+			cases.Type = "VIDE"
+		}
+	case 1:
+		switch cases.Type2 {
+		case "VIDE":
+			cases.Type2 = "ONTOP"
+		case "ONTOP":
+			cases.Type2 = "ONBOTTOM"
+		case "ONBOTTOM":
+			cases.Type2 = "ONLEFT"
+		case "ONLEFT":
+			cases.Type2 = "ONRIGHT"
+		case "ONRIGHT":
+			cases.Type2 = "VIDE"
+		}
+	case 2:
+		switch cases.Type3 {
+		case "VIDE":
+			cases.Type3 = "ONTOP"
+		case "ONTOP":
+			cases.Type3 = "ONBOTTOM"
+		case "ONBOTTOM":
+			cases.Type3 = "ONLEFT"
+		case "ONLEFT":
+			cases.Type3 = "ONRIGHT"
+		case "ONRIGHT":
+			cases.Type3 = "VIDE"
+		}
+	case 3:
+		cases.Flower = !cases.Flower
+	case 4:
+		cases.Item = !cases.Item
 	}
+}
+
+func Couche(w http.ResponseWriter, r *http.Request, grille *Grille) {
+	grille.Couche, _ = strconv.Atoi(r.FormValue("couche"))
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func CaculNombreItem(grid [][]Case) int {
+	items := 0
+	for _, item := range grid {
+		for _, cases := range item {
+			if cases.Item {
+				items++
+			}
+		}
+	}
+	return items
 }
